91d15f478d2512db5e3177d3ad01c9cf3df27cf6
jamesdanielwhitford <james.whitford0@gmail.com>
2025-06-13 08:06:07 +0200

    Update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,6 @@ module.exports = {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
       },
-      fontFamily: {
-        sans: ['var(--font-geist-sans)', 'system-ui', 'sans-serif'],
-        mono: ['var(--font-geist-mono)', 'monospace'],
-      },
     },
   },
   plugins: [],
